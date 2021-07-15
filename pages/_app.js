41228c9c5ab86e199a9f4428fbe7c89b27c67e8d
Guy Margalit <guy@margalit.com>
2021-07-14 18:28:23 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     padding: 0;
     margin: 0;
-    overflow: hidden;
+    height: 100vh;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
