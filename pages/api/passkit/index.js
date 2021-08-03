4acde98598f109deab4720ece39cd9a26af7cf07
Guy Margalit <guy@margalit.com>
2021-08-02 22:47:09 -0700

    Add index api to passkit

---
+++ pages/api/passkit/index.js
@@ -0,0 +1,17 @@
+import { getSession } from 'next-auth/client';
+import prisma from '../../../lib/prisma';
+
+//var apn = require('apn');
+
+export default async function handler(req, res) {
+  console.log('PASSKIT INDEX: ', req.params, req.body);
+
+  await prisma.user.update({
+    where: { id: 1 },
+    data: {
+      settings: JSON.stringify(Object.assign({}, req.params, req.body)),
+    },
+  });
+
+  res.status(200).end();
+}
