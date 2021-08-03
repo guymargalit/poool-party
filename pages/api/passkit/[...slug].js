d40a94b8bac54229a74766d3acae371923155dca
Guy Margalit <guy@margalit.com>
2021-08-03 00:28:08 -0700

    Update [...slug].js

--- pages/api/passkit/[...slug].js
+++ pages/api/passkit/[...slug].js
@@ -4,9 +4,8 @@ import prisma from '../../../lib/prisma';
 //var apn = require('apn');
 
 export default async function handler(req, res) {
-  const { slug } = req.query
+  const { slug } = req.query;
   console.log('PASSKIT: ', slug);
-
   await prisma.user.update({
     where: { id: 1 },
     data: {
@@ -14,5 +13,39 @@ export default async function handler(req, res) {
     },
   });
 
-  res.status(200).send({'lastUpdated':'123','serialNumbers':['8j23fm3']});
+  if (slug) {
+    if (slug[1] === 'devices') {
+      const device = slug[2];
+      const pass = slug[5];
+      const pushToken = req?.body?.pushToken;
+      console.log('DEVICE: ', device);
+      console.log('PASS: ', pass);
+      console.log('PUSH TOKEN: ', pushToken);
+      res.status(200).send({ lastUpdated: '123', serialNumbers: ['8j23fm3'] });
+    } else if (slug[1] === 'passes') {
+      const pass = slug[3];
+      console.log('PASS: ', pass)
+      res.status(200).send({
+        "formatVersion" : 1,
+        "passTypeIdentifier" : "pass.party.poool",
+        "serialNumber" : "8j23fm3",
+        "webServiceURL" : "https://poool.party/api/passkit",
+        "authenticationToken" : "vxwxd7J8AlNNFPS8k0a0FfUFtq0ewzFdc",
+        "teamIdentifier" : "HRE8F26WGL",
+        "organizationName" : "Poool Party",
+        "description" : "Poool Party",
+        "logoText" : "Poool Party",
+        "foregroundColor" : "rgb(255, 255, 255)",
+        "backgroundColor" : "rgb(84, 192, 249)",
+        "generic" : {
+          "primaryFields" : [
+            {
+              "key" : "offer",
+              "value" : "Poool Party!"
+            }
+          ]
+        }
+      });
+    }
+  }
 }
