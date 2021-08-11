29845fbf670d72b3965d09b924414df743f9d9a6
Guy Margalit <guy@margalit.com>
2021-08-11 15:16:59 -0600

    Disable lazy load connect

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -31,6 +31,7 @@ export default async function handler(req, res) {
     redis.set(`user-${session?.user?.id}`, JSON.stringify(updateUser));
     res.json(updateUser);
   } else if (req.method === 'GET') {
+    await prisma.$connect()
     const session = await getSession({ req });
     if (!session) {
       return res.status(403).end();
