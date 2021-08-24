7ba5b57beb29ac26fefe845efb41fe0707ba56c1
Guy Margalit <guy@margalit.com>
2021-08-24 13:12:59 -0700

    Update index.js

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -277,7 +277,7 @@ const Profile = ({ user, setVenmo, darkMode, setDarkMode }) => {
         {user?.venmo ? (
           <Link
             passHref
-            href={`venmo://${process.env.VENMO_CLIENT_ID}/login`}
+            href={`venmo://extension/login`}
           >
             <VenmoWrap link>
               <Info>
