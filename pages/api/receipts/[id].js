17461a0f5459317a16d0e87e308be8858af8d9f4
Guy Margalit <guy@margalit.com>
2021-08-29 16:07:40 -0700

    Fix receipt not in draft

--- pages/api/receipts/[id].js
+++ pages/api/receipts/[id].js
@@ -2,14 +2,21 @@ import prisma from '../../../lib/prisma';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const receipt = await prisma.receipt.findUnique({
+    const receipt = await prisma.receipt.findFirst({
       where: {
         id: req.query.id,
+        expense: {
+          draft: true,
+        }
       },
       select: {
         expense: true,
       },
     });
+
+    if (!receipt) {
+      return res.status(404).end();
+    }
     return res.json(receipt);
   }
 }
