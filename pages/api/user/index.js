1fdb2122a9a9d43b78486f53e50ed690a829e167
Guy Margalit <guy@margalit.com>
2021-09-04 11:19:07 -0700

    Remove user redis cache

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -37,44 +37,44 @@ export default async function handler(req, res) {
     if (!session) {
       return res.status(403).end();
     }
-    let cache;
-    if (redis) {
-      cache = await redis.get(`user-${session?.user?.id}`);
-      cache = JSON.parse(cache);
-    }
-    if (cache) {
-      const draft = await prisma.expense.findFirst({
-        where: { venmoId: cache?.venmo?.id, poolId: null, draft: true },
-      });
-      return res.status(200).json(Object.assign({}, cache, { draft }));
-    } else {
-      const user = await prisma.user.findUnique({
-        where: { id: session?.user?.id },
-        select: {
-          id: true,
-          name: true,
-          toy: true,
-          venmo: {
-            select: {
-              id: true,
-              username: true,
-              displayName: true,
-              image: true,
-              expiredAt: true,
-            },
+    // let cache;
+    // if (redis) {
+    //   cache = await redis.get(`user-${session?.user?.id}`);
+    //   cache = JSON.parse(cache);
+    // }
+    // if (cache) {
+    //   const draft = await prisma.expense.findFirst({
+    //     where: { venmoId: cache?.venmo?.id, poolId: null, draft: true },
+    //   });
+    //   return res.status(200).json(Object.assign({}, cache, { draft }));
+    // } else {
+    const user = await prisma.user.findUnique({
+      where: { id: session?.user?.id },
+      select: {
+        id: true,
+        name: true,
+        toy: true,
+        venmo: {
+          select: {
+            id: true,
+            username: true,
+            displayName: true,
+            image: true,
+            expiredAt: true,
           },
         },
-      });
+      },
+    });
 
-      if (redis) {
-        redis.set(`user-${session?.user?.id}`, JSON.stringify(user));
-      }
+    // if (redis) {
+    //   redis.set(`user-${session?.user?.id}`, JSON.stringify(user));
+    // }
 
-      const draft = await prisma.expense.findFirst({
-        where: { venmoId: user?.venmo?.id, poolId: null, draft: true },
-      });
-      return res.status(200).json(Object.assign({}, user, { draft }));
-    }
+    const draft = await prisma.expense.findFirst({
+      where: { venmoId: user?.venmo?.id, poolId: null, draft: true },
+    });
+    return res.status(200).json(Object.assign({}, user, { draft }));
+    // }
   } else if (req.method === 'PUT') {
     const session = await getSession({ req });
     if (!session) {
