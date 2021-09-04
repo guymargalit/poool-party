20d8d7c651e59598bbddcecf431e315947bf9c1a
Guy Margalit <guy@margalit.com>
2021-09-04 11:09:29 -0700

    Update index.js

--- pages/api/venmo/webhook/index.js
+++ pages/api/venmo/webhook/index.js
@@ -1,5 +1,4 @@
-import { getSession } from 'next-auth/client';
-import prisma from '../../../lib/prisma';
+import prisma from '../../../../lib/prisma';
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
