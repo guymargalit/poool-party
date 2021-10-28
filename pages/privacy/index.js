87814ccc1b63397d74ef09376f714a5d9e0aedca
Guy Margalit <guy@margalit.com>
2021-10-27 17:55:56 -0700

    Define router

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Logo } from '../../icons';
-// import Router from 'next/router';
+import Router from 'next/router';
 
 const Container = styled.div`
   display: flex;
