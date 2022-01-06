ad9218f0dde6921a737f68218d0119bf7d06f227
Guy Margalit <guy@margalit.com>
2022-01-06 09:10:37 -0800

    Styling fix

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -69,6 +69,9 @@ const Caption = styled.div`
   font-size: 18px;
   margin: 0.2rem 0.8rem;
   margin-top: 20px;
+  @media (max-width: 800px) {
+    margin-top: 0px;
+  }
   @media (max-width: 675px) {
     font-size: 14px;
   }
