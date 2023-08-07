4b1e5b867e0f415dea196ba796a9ebe910495b00
Guy Margalit <guymargalit@users.noreply.github.com>
2023-08-07 10:22:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 
 module.exports = withPWA({
-  webpack5: true,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    skipWaiting: true,
-  },
   images: {
     domains: [`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`],
   },
