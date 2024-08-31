94619d08e52c7e440a437824160c1cbae7a5aa13
Guy Margalit <guymargalit@users.noreply.github.com>
2024-08-31 13:44:46 -0700

    Update middleware runtime

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,5 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
+
+export const runtime = "edge";
